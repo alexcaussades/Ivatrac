3abb53c8ab8124aa2f01c4306535d19498cdeb71
alexcaussades <alexcaussades@gmail.com>
2023-07-03 21:10:15 +0200

    Favory users check (#21)
    * change visual (#18)
    
    * Add btn fav & Autre
    
    * fav_users

--- resources/views/metar-base.blade.php
+++ resources/views/metar-base.blade.php
@@ -7,10 +7,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> @yield("title")</title>
   <!-- CSS only -->
-  <link rel="stylesheet" href="{{ asset("asset/css/bootstrap.min.css") }} " type="text/css">
+  <link rel="stylesheet" href="{{ asset("/asset/css/bootstrap.min.css") }} " type="text/css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" >
   <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-  <link rel="stylesheet" href="{{ asset("asset/css/style.css") }} " type="text/css">
+  <link rel="stylesheet" href="{{ asset("/asset/css/style.css") }} " type="text/css">
 </head>
 @yield('navbar-metar')
 
