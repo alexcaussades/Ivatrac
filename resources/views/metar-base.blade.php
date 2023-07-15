44ddba165651973f242c13f0af0770441ff2a52f
alexcaussades <alexcaussades@gmail.com>
2023-07-15 10:56:02 +0200

    new structure

--- resources/views/metar-base.blade.php
+++ resources/views/metar-base.blade.php
@@ -5,11 +5,18 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title> @yield("title")</title>
+  <title> @yield("title") | Alexcaussades - IVAO - INFO</title>
   <!-- CSS only -->
+  @if (ENV('APP_ENV') == 'local')
   <link rel="stylesheet" href="{{ asset("asset/css/bootstrap.min.css") }} " type="text/css">
   <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset("asset/css/style.css") }} " type="text/css">
+  @else
+  <link rel="stylesheet" href="{{ asset("public/asset/css/bootstrap.min.css") }} " type="text/css">
+  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+  <link rel="stylesheet" href="{{ asset("public/asset/css/style.css") }} " type="text/css">
+  @endif
+
 </head>
 @yield('navbar-metar')
 
