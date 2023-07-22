5aa526396dae1b145ec2453997813a660aef690e
alexcaussades <alexcaussades@gmail.com>
2023-07-22 19:42:52 +0200

    New index pages (#35)

---
+++ resources/views/fpl/index.blade.php
@@ -0,0 +1,13 @@
+@extends("metar-base")
+
+@section("title", "Flight Plan System")
+
+
+@include("navbar")
+
+@section('content')
+
+<div class="container">
+    <h3 class="mt-2">Flight Plan System</h3>
+
+    
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,12 +15,25 @@
 <div class="container">
     @auth
     <h4 class="mt-3"> Welcome {{ auth()->user()->name }} ({{auth()->user()->vid}}) </h4>
+    <div class="d-flex flex-row">
+        <a href="{{ Route("serveur.index") }}" title="My account the website"><button type="submit" class="btn btn-primary btn-sm" >My account</button></a>
+        <a href="{{ Route("auth.logout") }}" title="Disconect the website"><button type="submit" class="btn btn-danger btn-sm" >Logout</button></a>
+        <!-- futur condition pour afficher si des amis sont en ligne est le nombres de en ce moment même  -->
+        <a href="http://"><button type="button" class="btn btn-secondary btn-sm ms-2" disabled> Online Friends</button></a>
+        <a href="https://webeye.ivao.aero/" target="_blank"><button type="button" class="btn btn-info btn-sm ms-2"> Webeye</button></a>
+        <a href="https://www.ivao.aero/" target="_blank"><button type="button" class="btn btn-info btn-sm ms-2"> IVAO Website</button></a>
+        <a href=""><button type="button" class="btn btn-primary btn-sm ms-2"> My FPL</button></a>
+    </div>
     @endauth
     @guest
-    <div class="d-flex flex-row-reverse">
-        <form action="{{ Route("auth.login") }}" method="get">
-            <button type="submit" class="btn btn-primary mt-2" title="Connect your account">Login</button>
-        </form>
+    <div class="d-flex flex-row mt-2">
+        <a href="{{ Route("auth.login") }}"><button type="submit" class="btn btn-primary btn-sm" title="Disconnect your account">Login / Register</button></a>
+        <!-- futur condition pour afficher si des amis sont en ligne est le nombres de en ce moment même  -->
+        <a href="http://" title="register on the platform only"><button type="button" class="btn btn-secondary btn-sm ms-2" disabled> Online Friends</button></a>
+        <a href="https://webeye.ivao.aero/" target="_blank"><button type="button" class="btn btn-info btn-sm ms-2"> Webeye</button></a>
+        <a href="https://www.ivao.aero/" target="_blank"><button type="button" class="btn btn-info btn-sm ms-2"> IVAO Website</button></a>
+        <a href="" title="register on the platform only"><button type="button" class="btn btn-secondary btn-sm ms-2" disabled> My FPL</button></a>
+
     </div>
     @endguest
     <div class="row mt-5">
@@ -38,14 +51,19 @@
                 <div class="card-body bg-dark border-dark text-white text-opacity-75">
                     <h4 class="card-title text-center">Pirep</h4>
                     <p class="card-text d-flex align-items-center"><span class="material-symbols-outlined">arrow_forward_ios</span> Register and store the pirep (FPL)</p>
+                    @auth
                     <p class="card-footer text-center"><a href="{{ Route("pirep.index")}}" class="btn btn-success">Creat FPL</a></p>
+                    @endauth
+                    @guest
+                    <p class="card-footer text-center"><a href="{{ Route("auth.login")}}" title="register on the platform only" class="btn btn-secondary">Creat FPL</a></p>
+                    @endguest
                 </div>
             </div>
         </div>
     </div>
     <hr>
     <div class="row">
-        <div class="d-flex align-items-center align-items-end "><span class="material-symbols-outlined">database </span> Version: {{$idlast["id"]}}  <span class="material-symbols-outlined ms-2">update</span> Next update at: {{$heurechange}} UTC</div>
+        <div class="d-flex align-items-center align-items-end "><span class="material-symbols-outlined">database </span> Version: {{$idlast["id"]}} <span class="material-symbols-outlined ms-2">update</span> Next update at: {{$heurechange}} UTC</div>
         <div class="col-4 mt-2">
             <div class="card text-white bg-dark">
                 <div class="card-body">
