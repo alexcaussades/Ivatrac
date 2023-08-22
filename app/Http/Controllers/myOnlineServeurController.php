b8a2588506b72b357d172c38cd831514381516b6
alexcaussades <alexcaussades@gmail.com>
2023-08-22 23:20:33 +0200

    Mode online (#43)
    * modification majeur
    
    * favory users
    
    * add frendly
    
    * update mineur
    
    * Friends final version
    
    * local environement
    
    * Mode Online
    
    * Modification mode online
    
    * Online vid and id users
    
    * Online Pilots

---
+++ app/Http/Controllers/myOnlineServeurController.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\metarController;
+
+class myOnlineServeurController extends Controller
+{
+    public $vid;
+    public $id;
+    public $option;
+
+
+    public function __construct($id, $vid = null)
+    {
+        $this->vid = $vid ?? null;
+        $this->id = $id;
+    }
+
+    public function get_VID()
+    {
+        $myOnlineServeur = new usersController();
+        $users = $myOnlineServeur->get_info_user($this->id);
+        return $users->vid;
+    }
+
+    public function get_whazzup()
+    {
+        $whazzup = new whazzupController();
+        $whazzup = $whazzup->get_whazzup();
+        return $whazzup;
+    }
+
+    public function VerrifOnlineServeur()
+    {
+        $whazzup = new whazzupController();
+        $whazzup = $whazzup->getwhazzupbdd();
+        $review = Storage::get('public/whazzup/' . $whazzup['whazzup'] . '.json');
+        $json = json_decode($review, true);
+        $atcs = collect($json['clients']['atcs']);
+        $pilots = collect($json['clients']['pilots']);
+        /** search the informations */
+        $atcs = $atcs->whereIn('userId', $this->vid);
+        $pilots = $pilots->whereIn('userId', $this->vid);
+        $atcs = $atcs->toArray();
+        $pilots = $pilots->toArray();
+        /** joker dans la recherhe index */
+        $atcs = array_values($atcs) ?? null;
+        $pilots = array_values($pilots) ?? null;
+        $u = collect(["atc" => $atcs, "pilot" => $pilots]);
+        return $u;
+    }
+
+    public function getVerrifOnlineServeur()
+    {
+        $q = $this->VerrifOnlineServeur();
+
+        if ($q['atc'] != null) {
+            $time = Carbon::parse($q['atc'][0]['lastTrack']["time"])->format('H:i');
+            $atis = count($q['atc'][0]['atis']['lines']);
+            $callsign = $q['atc'][0]['callsign'];
+            $callsign = explode("_", $callsign);
+            $callsign = $callsign[0];
+            $atc = [
+                "callsign" => $q['atc'][0]['callsign'],
+                "id_session" => $q['atc'][0]['id'],
+                "frequency" => $q['atc'][0]["atcSession"]['frequency'],
+                "rating" => $q['atc'][0]['rating'],
+                "time" => $time,
+                "revision" => $q['atc'][0]['atis']['revision'],
+                "atis" => $q['atc'][0]['atis']['lines'],
+            ];
+            $metarController = new metarController();
+            $plateform = [
+                "plateform" => [
+                    "APP" => $metarController->getApiATC_APP($callsign),
+                    "TWR" => $metarController->getApiATC_TWR($callsign),
+                    "GND" => $metarController->getApiATC_GND($callsign),
+                    "FSS" => $metarController->getApiATC_FSS($callsign),
+                ],
+            ];
+                $pilots = new PilotIvaoController();
+                $pilots = $pilots->getAirplaneToPilots($callsign);
+                
+            $fly = [
+                "fly" => [
+                    "departure" => [
+                        "count" => $pilots["departure"]["count"],
+                        "data" => $pilots["departure"]["data"]
+                    ],
+                    "arrivals" => [
+                        "count" => $pilots["arrivals"]["count"],
+                        "data" => $pilots["arrivals"]["data"]
+                    ]
+                ]
+            ];
+            
+            return view("myoline.atc", ["atc" => $atc, "atis" => $atis, "plateform" => $plateform, "fly" => $fly]);
+        }
+        elseif ($q['pilot'] != null) {
+            $q = $q['pilot'];
+            $distance_arrival = $q[0]['lastTrack']['arrivalDistance'];
+            $distance_arrival = explode(".", $distance_arrival);
+            $speed = $q[0]['lastTrack']['groundSpeed'] / 60;
+            if ($speed <= 0) {
+                $speed = 1;
+            }
+            if ($distance_arrival <= 0) {
+                $distance_arrival[0] = 1;
+            }
+            $arrival_time = $distance_arrival[0] / $speed;
+            $arrival_time = Carbon::now()->addMinutes($arrival_time)->format('H:i');
+
+            $p = [
+                "callsign" => $q[0]['callsign'],
+                "id_session" => $q[0]['id'],
+                "lastTrack" => [
+                    "altitude" => $q[0]['lastTrack']['altitude'],
+                    "transponder" => $q[0]['lastTrack']['transponder'],
+                    "arrivalDistance" => $distance_arrival[0],
+                    "state" => $q[0]['lastTrack']['state'],
+                    "time" => Carbon::parse($q[0]["lastTrack"]["time"])->format('H:i'),
+                ],
+                "flightPlan" => [
+                    "aircraftId" => $q[0]['flightPlan']['aircraftId'],
+                    "departureId" => $q[0]['flightPlan']['departureId'],
+                    "arrivalId" => $q[0]['flightPlan']['arrivalId'],
+                    "alternateId" => $q[0]['flightPlan']['alternativeId'],
+                    "route" => $q[0]['flightPlan']['route'],
+                    "speed" => $q[0]['flightPlan']['speed'],
+                    "level" => $q[0]['flightPlan']['level'],
+                    "flightRules" => $q[0]['flightPlan']['flightRules'],
+                    "flightType" => $q[0]['flightPlan']['flightType'],
+                    "personsOnBoard" => $q[0]['flightPlan']['peopleOnBoard'],
+                    "departureTime" => Carbon::parse($q[0]['flightPlan']['departureTime'])->format('H:i'),
+                    "aircraftEquipments" => $q[0]['flightPlan']['aircraftEquipments'],
+                    "aircraftTransponderTypes" => $q[0]['flightPlan']['aircraftTransponderTypes']
+                ],
+                "arrival_time" => $arrival_time,
+                "distance_arrival" => $distance_arrival[0],
+                "aircraft" => $q[0]['flightPlan']['aircraft']['model'],
+                "wakeTurbulence" => $q[0]['flightPlan']['aircraft']['wakeTurbulence']
+            ];
+            $metarController = new metarController();
+            //$departure = $metarController->metar($p["flightPlan"]["departureId"]);
+            //$arrival = $metarController->metar($p["flightPlan"]["arrivalId"]);
+            $atc = [
+                "depature" => [
+                    "APP" => $metarController->getApiATC_APP($p["flightPlan"]["departureId"]),
+                    "TWR" => $metarController->getApiATC_TWR($p["flightPlan"]["departureId"]),
+                    "GND" => $metarController->getApiATC_GND($p["flightPlan"]["departureId"]),
+                    "FSS" => $metarController->getApiATC_FSS($p["flightPlan"]["departureId"]),
+                ],
+                "arrival" => [
+                    "APP" => $metarController->getApiATC_APP($p["flightPlan"]["arrivalId"]),
+                    "TWR" => $metarController->getApiATC_TWR($p["flightPlan"]["arrivalId"]),
+                    "GND" => $metarController->getApiATC_GND($p["flightPlan"]["arrivalId"]),
+                    "FSS" => $metarController->getApiATC_FSS($p["flightPlan"]["arrivalId"]),
+                ]
+            ];
+            $chartController = new chartController();
+            $chart = [
+                "departure" => [
+                    "IFR" => $chartController->chartIFR($p["flightPlan"]["departureId"]),
+                    "VFR" => $chartController->chartVFR($p["flightPlan"]["departureId"]),
+                ],
+                "arrival" => [
+                    "IFR" => $chartController->chartIFR($p["flightPlan"]["arrivalId"]),
+                    "VFR" => $chartController->chartVFR($p["flightPlan"]["arrivalId"]),
+                ]
+            ];
+
+            return view("myoline.pilot", ["pilot" => $p, "atc" => $atc, "chart" => $chart]);
+        } else {
+            return redirect()->route("home");
+        }
+    }
+
+    public function check_online()
+    {
+        $q = $this->VerrifOnlineServeur();
+
+        if($q["atc"] != null){
+            return true;
+        }
+        elseif($q["pilot"] != null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
---
+++ resources/views/myoline/pilot.blade.php
@@ -0,0 +1,192 @@
+@extends("metar-base")
+
+@section("title", "Online")
+
+
+@include("navbar")
+
+@section('content')
+
+<div class="container mt-5">
+    <h2>{{$pilot["callsign"]}}</h2>
+    <div class="text-muted">Tracker Id : {{$pilot['id_session']}}</div>
+
+    <div class="card text-white bg-dark">
+        <div class="card-body">
+            <h4 class="card-title d-flex align-items-center">On Live <span class="material-symbols-outlined text-danger ms-2 blink">radio_button_checked</span></h4>
+            <p class="card-text">
+            <div class="row">
+                <div class="col"> <span class="text-info">Altitude :</span> {{ $pilot["lastTrack"]["altitude"] }} ft </div>
+                <div class="col"> <span class="text-info">Transponder :</span> {{ $pilot["lastTrack"]["transponder"] }} </div>
+                <div class="col"> <span class="text-info">Arrival Distance :</span> {{ $pilot["lastTrack"]["arrivalDistance"] }} NM</div>
+                <div class="col"> <span class="text-info">ETA : </span>{{ $pilot["arrival_time"] }} UTC </div>
+                <div class="col"> <span class="text-info">State : </span>{{ $pilot["lastTrack"]["state"] }} </div>
+                <div class="col"> <span class="text-info">Time Online : </span>{{ $pilot["lastTrack"]["time"] }} </div>
+            </div>
+            </p>
+        </div>
+    </div>
+    <hr>
+    <!-- Service ATCS -->
+    <div class="card text-white bg-dark">
+        <div class="card-body">
+            <h4 class="card-title">Services ATC</h4>
+            <p class="card-text">
+            <div class="row">
+                <div class="col">
+                    <span class="text-info">Departure :</span><br>
+                </div>
+                <div class="col">
+                    <span class="text-info">Arrival :</span><br>
+                </div>
+            </div>
+            <div class="row">
+                <!-- Information des ATCs sur le depart -->
+                <div class="col mt-2">
+                    @if ($atc["depature"]["APP"] == null)
+                    @else
+                    <ul><button class="btn btn-success btn-sm">{{ $atc["depature"]["APP"]["callsign"] }} - {{ $atc["depature"]["APP"]["atcSession"]["frequency"] }} Mhz</button></ul>
+                    @endif
+                    @if ($atc["depature"]["TWR"] == null)
+                    @else
+                    <ul><button class="btn btn-success btn-sm">{{ $atc["depature"]["TWR"]["callsign"] }} - {{ $atc["depature"]["TWR"]["atcSession"]["frequency"] }} Mhz</button></ul>
+                    @endif
+                    @if ($atc["depature"]["GND"] == null)
+
+                    @else
+                    <ul><button class="btn btn-success btn-sm">{{ $atc["depature"]["GND"]["callsign"] }} - {{ $atc["depature"]["GND"]["atcSession"]["frequency"] }} Mhz</button></ul>
+                    @endif
+                    @if ($atc["depature"]["FSS"] == null)
+
+                    @else
+                    <ul><button class="btn btn-success btn-sm">{{ $atc["depature"]["FSS"]["callsign"] }} - {{ $atc["depature"]["FSS"]["atcSession"]["frequency"] }} Mhz</button></ul>
+                    @endif
+                </div>
+                <!-- Information des ATCs sur l'arrivée -->
+                <div class="col mt-2">
+                    @if ($atc["arrival"]["APP"] == null)
+                    @else
+                    <ul><button class="btn btn-success btn-sm">{{ $atc["arrival"]["APP"]["callsign"] }} - {{ $atc["arrival"]["APP"]["atcSession"]["frequency"] }} Mhz</button></ul>
+                    @endif
+                    @if ($atc["arrival"]["TWR"] == null)
+                    @else
+                    <ul><button class="btn btn-success btn-sm">{{ $atc["arrival"]["TWR"]["callsign"] }} - {{ $atc["arrival"]["TWR"]["atcSession"]["frequency"] }} Mhz</button></ul>
+                    @endif
+                    @if ($atc["arrival"]["GND"] == null)
+                    @else
+                    <ul><button class="btn btn-success btn-sm">{{ $atc["arrival"]["GND"]["callsign"] }} - {{ $atc["arrival"]["GND"]["atcSession"]["frequency"] }} Mhz</button></ul>
+                    @endif
+                    @if ($atc["arrival"]["FSS"] == null)
+                    @else
+                    <ul><button class="btn btn-success btn-sm">{{ $atc["arrival"]["FSS"]["callsign"] }} - {{ $atc["arrival"]["FSS"]["atcSession"]["frequency"] }} Mhz</button></ul>
+                    @endif
+                </div>
+            </div>
+            <!-- Cartes des aréodromes -->
+            @if ($chart["departure"]["IFR"] == null)
+
+            @else
+            <div class="row">
+                <div class="col">
+                    <span class="text-info">Chart :</span><br>
+                    <a href="{{$chart["departure"]["IFR"]}}" target="_blank"> <button type="submit" class="btn btn-info text-dark ms-5"><span class="d-flex align-items-center"><span class="material-symbols-outlined">assistant_navigation</span>&nbsp IFR</span></button></a>
+                    <a href="{{$chart["departure"]["VFR"]}}" target="_blank"> <button type="submit" class="btn btn-info text-dark ms-2"><span class="d-flex align-items-center"><span class="material-symbols-outlined">navigation</span> VFR</span></button></a>
+                </div>
+                <div class="col">
+                    <span class="text-info">Chart :</span><br>
+                    <a href="{{$chart["arrival"]["IFR"]}}" target="_blank"> <button type="submit" class="btn btn-info text-dark ms-5"><span class="d-flex align-items-center"><span class="material-symbols-outlined">assistant_navigation</span>&nbsp IFR</span></button></a>
+                    <a href="{{$chart["arrival"]["VFR"]}}" target="_blank"> <button type="submit" class="btn btn-info text-dark ms-2"><span class="d-flex align-items-center"><span class="material-symbols-outlined">navigation</span> VFR</span></button></a>
+                </div>
+            </div>
+            @endif
+            </p>
+        </div>
+    </div>
+    <hr>
+    <!-- Détails du plans de vol -->
+    <div>
+        <div class="row mt-2">
+            <div class="col">
+                <div class="form-group">
+                    <label for="" class="text-primary">Departure :</label>
+                    <input type="text" class="form-control" value="{{ $pilot["flightPlan"]['departureId'] }}" disabled>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="" class="text-primary">Arrival :</label>
+                    <input type="text" class="form-control" value="{{ $pilot["flightPlan"]['arrivalId'] }}" disabled>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="" class="text-primary">Alternate :</label>
+                    <input type="text" class="form-control" value="{{ $pilot["flightPlan"]['alternateId'] }}" disabled>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="" class="text-primary">flight Rules : </label>
+                    <input type="text" class="form-control" value="{{ $pilot["flightPlan"]['flightRules'] }}" disabled>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-2">
+            <div class="col">
+                <div class="form-group">
+                    <label for="" class="text-primary">Speed :</label>
+                    <input type="text" class="form-control" value="{{ $pilot["flightPlan"]['speed'] }}" disabled>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="" class="text-primary">Level (FL) :</label>
+                    <input type="text" class="form-control" value="{{ $pilot["flightPlan"]['level'] }}" disabled>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="" class="text-primary">POB : </label>
+                    <input type="text" class="form-control" value="{{ $pilot["flightPlan"]['personsOnBoard'] }}" disabled>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="" class="text-primary">Depature Time : (UTC) </label>
+                    <input type="text" class="form-control" value="{{ $pilot["flightPlan"]['departureTime'] }}" disabled>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="col-2">
+                <div class="form-group">
+                    <label for="" class="text-primary">Aircraft :</label>
+                    <input type="text" class="form-control" value="{{ $pilot["aircraft"]}}" disabled>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="" class="text-primary">Aircraft Equipments :</label>
+                    <input type="text" class="form-control" value="{{ $pilot["flightPlan"]['aircraftEquipments'] }}" disabled>
+                </div>
+            </div>
+            <div class="col">
+
+            </div>
+        </div>
+
+        <div class="row mt-2">
+            <div class="col">
+                <label for="Route" class="text-primary">Route : </label>
+                <textarea class="form-control" rows="3" disabled>{{ $pilot["flightPlan"]["route"] }}</textarea>
+            </div>
+        </div>
+        <hr>
+    </div>
+
+    @if (ENV('APP_ENV') == 'local')
+    <script src="{{ asset("asset/js/update_friend.js") }}"></script>
+    @else
+    <script src="{{ asset("public/asset/js/update_friend.js") }}"></script>
+    @endif
