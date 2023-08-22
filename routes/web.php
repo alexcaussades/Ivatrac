5fa076f4b5062167db71944a46122aca0f78b8b2
alexcaussades <alexcaussades@gmail.com>
2023-08-22 23:37:17 +0200

    update online

--- routes/web.php
+++ routes/web.php
@@ -594,7 +594,7 @@ Route::prefix("friends")->group(function () {
 
 
 Route::get("online", function (Request $request) {
-    $online = new myOnlineServeurController(auth::user()->id, 695042);
+    $online = new myOnlineServeurController(auth::user()->id, auth::user()->vid);
     $online = $online->getVerrifOnlineServeur();
     return $online;
 })->name("online")->middleware(["auth:web"]);
