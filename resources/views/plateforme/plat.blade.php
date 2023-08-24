31b47a8cf331c138d5c977eb0dccd9d3949a84b4
alexcaussades <alexcaussades@gmail.com>
2023-08-24 16:48:30 +0200

    other

--- resources/views/plateforme/plat.blade.php
+++ resources/views/plateforme/plat.blade.php
@@ -1,3 +1,7 @@
+<?php
+
+use Illuminate\Support\Carbon;
+?>
 @extends("metar-base")
 
 @section("title", "Info IVAO")
@@ -6,26 +10,27 @@
 
 @section('content')
 
+
 <div class="container">
     <a href="{{ Route('ivao.plateforme', ["icao" => $Pilot["icao"]] )}}" class="btn btn-success"><span class="d-flex align-items-center"><span class="material-symbols-outlined">sync</span> Refresh</span></a>
-    <div class="row mt-2">
+    <div class="row mt-2 d-flex justify-content-center">
         @if ($ivao["APP"] != null)
         <div class="col">
             <div class="card text-white bg-dark mb-3">
                 <div class="card-header text-center text-warning">
                     <h4>{{ $ivao["APP"]["callsign"]}}</h4>
+                    <span class="text-info">Online: {{Carbon::parse($ivao["APP"]["lastTrack"]["time"])->format('H:i')}}</span>
                 </div>
                 <div class="card-body text-center">
                     Freq: {{ $ivao["APP"]["atcSession"]["frequency"] }} Mhz
                     <hr>
                     Revision: {{$ivao["APP"]["atis"]["revision"]}}
                     <hr>
-                    {{$ATC["APP"][0]}}
-                    <hr>
-                    {{$ATC["APP"][1]}}
-                    <hr>
-                    {{$ATC["APP"][2]}} {{$ATC["APP"][3] ?? ""}}
+                    {{$ATC["APP"][0] ?? ""}} / {{$ATC["APP"][1] ?? ""}}
                 </div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <!-- <a href="{{ Route("friends.add.oter.page.post", ["vid_friend" => $ivao['APP']["userId"], "host" => $hosturl ] ) }}"><button type="button" class="btn btn-info">Add Friend</button></a> -->
             </div>
         </div>
         @endif
@@ -34,17 +39,14 @@
             <div class="card text-white bg-dark mb-3">
                 <div class="card-header text-center text-warning">
                     <h4>{{ $ivao["TWR"]["callsign"]}}</h4>
+                    <span class="text-info">Online: {{Carbon::parse($ivao["TWR"]["lastTrack"]["time"])->format('H:i')}}</span>
                 </div>
                 <div class="card-body text-center">
                     Freq: {{ $ivao["TWR"]["atcSession"]["frequency"] }} Mhz
                     <hr>
                     Revision: {{$ivao["TWR"]["atis"]["revision"]}}
                     <hr>
-                    {{$ATC["TWR"][0]}}
-                    <hr>
-                    {{$ATC["TWR"][1]}}
-                    <hr>
-                    {{$ATC["TWR"][2]}} {{$ATC["TWR"][3] ?? ""}}
+                    {{$ATC["TWR"][0] ?? ""}} / {{$ATC["TWR"][1] ?? ""}}
                 </div>
             </div>
         </div>
@@ -54,17 +56,14 @@
             <div class="card text-white bg-dark mb-3">
                 <div class="card-header text-center text-warning">
                     <h4>{{ $ivao["GND"]["callsign"]}}</h4>
+                    <span class="text-info">Online: {{Carbon::parse($ivao["GND"]["lastTrack"]["time"])->format('H:i')}}</span>
                 </div>
                 <div class="card-body text-center">
                     Freq: {{ $ivao["GND"]["atcSession"]["frequency"] }} Mhz
                     <hr>
                     Revision: {{$ivao["GND"]["atis"]["revision"]}}
                     <hr>
-                    {{$ATC["GND"][0]}}
-                    <hr>
-                    {{$ATC["GND"][1]}}
-                    <hr>
-                    {{$ATC["GND"][2]}} {{$ATC["GND"][3] ?? ""}}
+                    {{$ATC["GND"][0] ?? ""}} / {{$ATC["GND"][1] ?? ""}}
                 </div>
             </div>
         </div>
@@ -74,32 +73,32 @@
             <div class="card text-white bg-dark mb-3">
                 <div class="card-header text-center text-warning">
                     <h4>{{ $ivao["FSS"]["callsign"]}}</h4>
+                    <span class="text-info">Online: {{Carbon::parse($ivao["FSS"]["lastTrack"]["time"])->format('H:i')}}</span>
                 </div>
                 <div class="card-body text-center">
                     Freq: {{ $ivao["FSS"]["atcSession"]["frequency"] }} Mhz
                     <hr>
                     Revision: {{$ivao["FSS"]["atis"]["revision"]}}
                     <hr>
-                    {{$ATC["FSS"][0]}}
-                    <hr>
-                    {{$ATC["FSS"][1]}}
-                    <hr>
-                    {{$ATC["FSS"][2]}} {{$ATC["FSS"][3] ?? ""}}
-                </div>
-            </div>
-        </div>
-        @endif
-        @if($ivao["APP"] == null && $ivao["TWR"] == null && $ivao["GND"] == null && $ivao["FSS"] == null)
-        <div class="col">
-            <div class="card text-white bg-dark mb-3">
-                <div class="card-header text-center text-warning">
-                    <h4>No ATC</h4>
+                    {{$ATC["FSS"][0] ?? ""}} / {{$ATC["FSS"][1] ?? ""}}
                 </div>
             </div>
         </div>
         @endif
     </div>
     <hr>
+    <div class="row mt-2 d-flex justify-content-center">
+        <div class="col-12">
+            @foreach ($other as $r )
+            @for ($i = 0; $i < count($r); $i++) <div class="d-flex d-inline-flex">
+                <button class="btn btn-info">{{$r[$i]["callsign"]}}</button>
+        </div>
+        @endfor
+        @endforeach
+    </div>
+</div>
+<hr>
+<div class="">
     <div class="col">
         <h5><span class="material-symbols-outlined">flight_takeoff</span> Departure ({{$Pilot["departure"]["count"]}})</h5>
         <table class="table table-striped table-inverse table-responsive">
@@ -110,6 +109,7 @@
                     <th>Route</th>
                     <th>FlightRules</th>
                     <th>TO</th>
+                    <th>Online</th>
                 </tr>
             </thead>
             <tbody>
@@ -121,6 +121,7 @@
                     <td>{{ $PilotData["flightPlan"]["route"] }}</td>
                     <td>{{ $PilotData["flightPlan"]["flightRules"] }}</td>
                     <td>{{ $PilotData["flightPlan"]["arrivalId"] }}</td>
+                    <td>{{ Carbon::parse($PilotData["lastTrack"]["time"])->format('H:i') }}</td>
                 </tr>
 
                 @endforeach
@@ -137,10 +138,16 @@
                     <th>Route</th>
                     <th>FlightRules</th>
                     <th>FROM</th>
+                    <th>Arrived in</th>
+                    <th>Online</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($Pilot["arrivals"]["data"] as $PilotData)
+                @php
+                $distance_arrival = $PilotData['lastTrack']['arrivalDistance'];
+                $distance_arrival = explode(".", $distance_arrival);
+                @endphp
 
                 <tr>
                     <td scope="row">{{ $PilotData["callsign"] }}</td>
@@ -148,6 +155,8 @@
                     <td>{{ $PilotData["flightPlan"]["route"] }}</td>
                     <td>{{ $PilotData["flightPlan"]["flightRules"] }}</td>
                     <td>{{ $PilotData["flightPlan"]["departureId"] }}</td>
+                    <td>{{ $distance_arrival[0] }} NM</td>
+                    <td>{{ Carbon::parse($PilotData["lastTrack"]["time"])->format('H:i') }}</td>
                 </tr>
 
                 @endforeach
@@ -156,3 +165,9 @@
     </div>
 </div>
 </div>
+
+@if (ENV('APP_ENV') == 'local')
+<script src="{{ asset("asset/js/update_friend.js") }}"></script>
+@else
+<script src="{{ asset("public/asset/js/update_friend.js") }}"></script>
+@endif
