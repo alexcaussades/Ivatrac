09f96913605111f85fd505d239d93b493ab698cf
alexcaussades <alexcaussades@gmail.com>
2023-09-19 19:54:51 +0200

    s

--- app/Http/Controllers/AuthIVAOController.php
+++ app/Http/Controllers/AuthIVAOController.php
@@ -113,7 +113,7 @@ class AuthIVAOController extends Controller
                 $user_context
             );
             $user_res_data = json_decode($user_result, true);
-            if (isset($user_res_data)) {
+            if ($user_res_data["id"] != null) {
                 $request->merge([
                     "id" => $user_res_data["id"],
                 ]);
