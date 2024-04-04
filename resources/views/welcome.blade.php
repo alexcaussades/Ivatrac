3b9c981caaa7381009fb0e4f9603397bfb00c684
alexcaussades <alexcaussades@gmail.com>
2024-04-04 06:51:10 +0200

    patch acceuil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -166,30 +166,32 @@ use Illuminate\Support\Carbon;
         
         <div class="row">
             @foreach($event_fr as $events)
+                    @if (isset($events[0]["type"]) == "training" || isset($events[0]["type"]) == "exam")
+                        <div class="col-6 mt-2">
+                        <div class="card text-white bg-dark">
+                            <div class="card-body">
+                                @if ($events[0]["type"] == "training")
+                                    @if (ENV('APP_ENV') == 'local')
+                                    <img class="card-img-top" src="{{ asset("asset/img/training/training.png") }}" alt="">
+                                    @else
+                                    <img class="card-img-top" src="{{ asset("public/asset/img/training/training.png") }}" alt="">
+                                    @endif  
+                                @endif
+                                @if ($events[0]["type"] == "exam")
+                                    @if (ENV('APP_ENV') == 'local')
+                                    <img class="card-img-top" src="{{ asset("asset/img/exam/exam.png") }}" alt="">
+                                    @else
+                                    <img class="card-img-top" src="{{ asset("public/asset/img/exam/exam.png") }}" alt="">
+                                    @endif  
+                                @endif
+                                <p class="card-text text-center text-info mt-2">{{$events[0]["name"]}}</p>
+                                <p class="card-text text-center ">{{$events[0]["description"]}}</p>
+                                <p class="card-text text-center "><button class="btn btn-outline-success">{{Carbon::parse($events[0]["started_at"])->format('d-m H:i') }}Z</button> </p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
             
-            <div class="col-6 mt-2">
-                <div class="card text-white bg-dark">
-                    <div class="card-body">
-                        @if ($events[0]["type"] == "training")
-                            @if (ENV('APP_ENV') == 'local')
-                            <img class="card-img-top" src="{{ asset("asset/img/training/training.png") }}" alt="">
-                            @else
-                            <img class="card-img-top" src="{{ asset("public/asset/img/training/training.png") }}" alt="">
-                            @endif  
-                        @endif
-                        @if ($events[0]["type"] == "exam")
-                            @if (ENV('APP_ENV') == 'local')
-                            <img class="card-img-top" src="{{ asset("asset/img/exam/exam.png") }}" alt="">
-                            @else
-                            <img class="card-img-top" src="{{ asset("public/asset/img/exam/exam.png") }}" alt="">
-                            @endif  
-                        @endif
-                        <p class="card-text text-center text-info mt-2">{{$events[0]["name"]}}</p>
-                        <p class="card-text text-center ">{{$events[0]["description"]}}</p>
-                        <p class="card-text text-center "><button class="btn btn-outline-success">{{Carbon::parse($events[0]["started_at"])->format('d-m H:i') }}Z</button> </p>
-                    </div>
-                </div>
-            </div>
             @endforeach
         </div>
     </div>
